4fa8485935a7bf3d25ad042ab739d85add3f00de
supermarine1377 <supermarine@outlook.jp>
2022-12-18 17:51:43 +0900

    fix myans

--- easy/plus_one/myans/myans.go
+++ easy/plus_one/myans/myans.go
@@ -1,46 +1,21 @@
 package myans
 
-import "math"
-
 func plusOne(digits []int) []int {
-	num := convertToInt(digits)
-	num++
-	return convertToArr(num)
-}
-
-// [1, 2, 3] gives 123
-func convertToInt(nums []int) int {
-	var result int
-	for i, num := range nums {
-		result += num * pow10((len(nums) - i - 1))
+	len := len(digits)
+	if digits[len-1] != 9 {
+		digits[len-1] = digits[len-1] + 1
+		return digits
 	}
-
-	return result
-}
-
-// 123 gives [1, 2, 3]
-func convertToArr(num int) []int {
-	digit := digit(num)
-	result := make([]int, digit, digit)
-	for i := digit - 1; i > -1; i-- {
-		rem := num % 10
-		result[i] = rem
-		num = num / 10
-	}
-	return result
-}
-
-func pow10(num int) int {
-	return int(math.Pow10(num))
-}
-
-func digit(num int) int {
-	var result int
-	for {
-		if pow10(result) <= num && pow10(result+1) > num {
-			break
+	for i := len - 1; i > -1; i-- {
+		digit := digits[i]
+		if digit == 9 {
+			digits[i] = 0
+			continue
 		}
-		result++
+		digits[i] = digit + 1
+		return digits
 	}
-	return result + 1
+	newDigits := make([]int, len+1)
+	newDigits[0] = 1
+	return newDigits
 }
