03cb4cb215dba9d4857bab3ff666d37a03c37848
supermarine1377 <supermarine@outlook.jp>
2023-01-03 19:46:40 +0900

    add problem

--- easy/backspace_string_compare/myans.go
+++ easy/backspace_string_compare/myans.go
@@ -1,9 +1,11 @@
 package backspace_string_compare
 
+// if you use the fuction output() below, Time Complexity is O(N), and Space Complexity is O(len(s) + len(t))
 func backspaceCompare(s string, t string) bool {
 	return output(s) == output(t)
 }
 
+// Time Complexity: O(N), Space Complexity: O(len(s))
 func output(s string) string {
 	var result string
 	for _, r := range s {
