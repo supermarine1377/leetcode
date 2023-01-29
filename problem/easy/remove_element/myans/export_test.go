f1fb26dec73c5524d311dd05246f67b14e47e59a
supermarine1377 <supermarine@outlook.jp>
2023-01-29 14:17:51 +0900

    add solution

--- problem/easy/remove_element/myans/export_test.go
+++ problem/easy/remove_element/myans/export_test.go
@@ -1,38 +1,82 @@
 package myans
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func Test_removeElement(t *testing.T) {
-	type args struct {
-		nums []int
-		Val  int
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int
-		wantArr []int
-	}{
+type args struct {
+	nums []int
+	val  int
+}
+
+type testcase struct {
+	name string
+	args args
+	want []int
+}
+
+func getTestCases() []testcase {
+	return []testcase{
 		{
 			name: "1st",
 			args: args{
-				nums: []int{3, 2, 2, 3},
-				Val:  3,
+				nums: nil,
+				val:  0,
 			},
-			want:    2,
-			wantArr: []int{2, 2, 2, 3},
+			want: nil,
+		},
+		{
+			name: "2nd",
+			args: args{
+				nums: []int{1},
+				val:  1,
+			},
+			want: []int{},
+		},
+		{
+			name: "3rd",
+			args: args{
+				nums: []int{1},
+				val:  0,
+			},
+			want: []int{1},
+		},
+		{
+			name: "4th",
+			args: args{
+				nums: []int{3, 2, 2, 3},
+				val:  3,
+			},
+			want: []int{2, 2},
 		},
 	}
-	for _, tt := range tests {
+}
+
+func Test_removeElement(t *testing.T) {
+	for _, tt := range getTestCases() {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := removeElement(tt.args.nums, tt.args.Val); got != tt.want {
-				t.Errorf("removeElement() = %v, want %v", got, tt.want)
+			got := removeElement(tt.args.nums, tt.args.val)
+			// order dosen't matter
+			sort.Ints(got)
+			sort.Ints(tt.want)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("error: unexpected diff; %s", diff)
 			}
-			if diff := cmp.Diff(tt.args.nums, tt.wantArr); diff != "" {
+		})
+	}
+}
+
+func Test_removeElement_notUsingTwoPointer(t *testing.T) {
+	for _, tt := range getTestCases() {
+		t.Run(tt.name, func(t *testing.T) {
+			got := removeElement_notUsingTwoPointers(tt.args.nums, tt.args.val)
+			// order dosen't matter
+			sort.Ints(got)
+			sort.Ints(tt.want)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
 				t.Errorf("error: unexpected diff; %s", diff)
 			}
 		})
--- problem/easy/remove_element/myans/myans.go
+++ problem/easy/remove_element/myans/myans.go
@@ -1,12 +1,36 @@
 package myans
 
-func removeElement(nums []int, Val int) int {
+func removeElement(nums []int, val int) []int {
 	firstIndex := 0
 	for secondIndex, num := range nums {
-		if num != Val {
+		if num != val {
 			nums[firstIndex] = nums[secondIndex]
 			firstIndex++
 		}
 	}
-	return firstIndex
+	return nums[:firstIndex]
+}
+
+// This solution does repeted-work
+func removeElement_notUsingTwoPointers(nums []int, val int) []int {
+	if len(nums) == 0 {
+		return nil
+	}
+	var i int
+	for {
+		if nums[i] == val {
+			// remove element
+			nums = append(nums[:i], nums[i+1:]...)
+			if len(nums) == 0 {
+				return []int{}
+			}
+			i = 0
+			continue
+		}
+		if i == len(nums)-1 {
+			break
+		}
+		i++
+	}
+	return nums
 }
