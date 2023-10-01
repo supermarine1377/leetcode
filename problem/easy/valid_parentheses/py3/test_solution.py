eb7c83d58655cd7849ed816311875cde8faef74b
supermarine1377 <supermarine@outlook.jp>
2023-10-01 19:32:36 +0900

    [leetcode_20] fix: lint

--- problem/easy/valid_parentheses/py3/test_solution.py
+++ problem/easy/valid_parentheses/py3/test_solution.py
@@ -1,24 +1,25 @@
 import unittest
 from solution import Solution
 
+
 class TestSolution(unittest.TestCase):
-  def test_valid_parentheses(self):
-    solution = Solution()
-    testcases = [
-      ["()", True],
-      ["()[]{}", True],
-      ["(]", False],
-      ["((", False],
-      ["]", False],
-      [")(){}", False]
-    ]
-    for s, expected in testcases:
-      ans = solution.isValid(s)
-      if ans:
-        self.assertTrue(expected)
-      else:
-        self.assertFalse(expected)
-  
+    def test_valid_parentheses(self):
+        solution = Solution()
+        testcases = [
+            ["()", True],
+            ["()[]{}", True],
+            ["(]", False],
+            ["((", False],
+            ["]", False],
+            [")(){}", False]
+        ]
+        for s, expected in testcases:
+            ans = solution.isValid(s)
+            if ans:
+                self.assertTrue(expected)
+            else:
+                self.assertFalse(expected)
+
+
 if __name__ == "__main__":
     unittest.main()
-        
