98b71d315c97c5c035f9336828e10a6a9c097f27
supermarine1377 <supermarine@outlook.jp>
2024-03-20 18:00:31 +0900

    Fix import path in Python3 test function

--- problem/easy/remove_duplicates_from_sorted_array/test_myans.py
+++ problem/easy/remove_duplicates_from_sorted_array/test_myans.py
@@ -1,12 +1,12 @@
 import unittest
-import myans
+import myans as remove_duplicates_from_sorted_array_myans
 
 class Testbestans(unittest.TestCase):
   
     def test_remove_duplicates_from_sorted_array(self):
-        self.assertEqual(myans.Solution.removeDuplicates(self, [1, 1, 2]), [1,2])
-        self.assertEqual(myans.Solution.removeDuplicates(self, [1, 1, 2, 2, 2, 3, 3, 4]), [1,2,3,4])
-        self.assertEqual(myans.Solution.removeDuplicates(self, [1, 1, 1]), [1])
+        self.assertEqual(remove_duplicates_from_sorted_array_myans.Solution.removeDuplicates(self, [1, 1, 2]), [1,2])
+        self.assertEqual(remove_duplicates_from_sorted_array_myans.Solution.removeDuplicates(self, [1, 1, 2, 2, 2, 3, 3, 4]), [1,2,3,4])
+        self.assertEqual(remove_duplicates_from_sorted_array_myans.Solution.removeDuplicates(self, [1, 1, 1]), [1])
 
 if __name__ == "__main__":
     unittest.main()
